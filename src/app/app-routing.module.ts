737448f99892b2b15b5793c349d05b49463fa7da
benzarty <m.benzarti.1996@gmail.com>
2018-11-24 18:32:00 +0100

    lister

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,25 @@ import {RouterModule, Route, Routes} from '@angular/router';
 import {ServiceadminComponent} from './Serviceadmin/serviceadmin.component';
 import {HomeComponent} from './home/home.component';
 
+import {DashBoardComponent} from './dash-board/dash-board.component';
+import {ListerFormateurComponent} from './dash-board/formateur/lister-formateur/lister-formateur.component';
+import {AjouterFormateurComponent} from './dash-board/formateur/ajouter-formateur/ajouter-formateur.component';
+
+
 const routes: Routes = [
     {
       path: 'Serviceadmin', component: ServiceadminComponent
     }, {
       path: '',
       component: HomeComponent
-    }
+    },
+  {
+   path: 'dashboard', component: DashBoardComponent, children: [{
+     path: 'formateur/lister', component: ListerFormateurComponent
+    },{
+     path: 'formateur/ajouter', component: AjouterFormateurComponent
+    }]
+  }
   ]
 ;
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ServiceadminComponent } from './Serviceadmin/serviceadmin.component';
 import { HomeComponent } from './home/home.component';
+import { ListerFormateurComponent } from './dash-board/formateur/lister-formateur/lister-formateur.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AjouterFormateurComponent } from './dash-board/formateur/ajouter-formateur/ajouter-formateur.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServiceadminComponent,
-    HomeComponent
+    HomeComponent,
+    ListerFormateurComponent,
+    DashBoardComponent,
+    AjouterFormateurComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
